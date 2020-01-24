960b8c85b951d17f8a135117731ba3e7ccccbf6d
mittulmandhan <conanmittul@gmail.com>
2020-01-24 20:43:39 +0530

    added routing and pagenotfund component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { ProductsComponent } from './products/products.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    ProductsComponent
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
